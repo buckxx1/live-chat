23a6166a4e20671c1c5db4cc39147993cad556fd
Nicholas Hoban <nickslaptop@Nicholass-MBP.cogeco.local>
2022-03-08 15:48:53 -0500

    server shoukd be all up and running now

--- php/config.php
+++ php/config.php
@@ -1,5 +1,12 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "live-chat");
+// this was the development connnection 
+    //$conn = mysqli_connect("localhost", "root", "", "live-chat");
+    //if(!$conn){
+    //    echo "live-chat Database connected" . mysqli_connect_error//();
+    //} 
+
+    //remote deployed connection 
+    $conn = mysqli_connect("remotemysql.com", "p6AoBnRj5r", "eAACPt6lMr", "p6AoBnRj5r");
     if(!$conn){
         echo "live-chat Database connected" . mysqli_connect_error();
     } 
